8f1e8e11b16801717227da6a5adc5976976ece05
Ajgorek04 <igorbledzinski12@gmail.com>
2025-04-20 22:25:59 +0200

    updating src

---
+++ src/app/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div id="footer">
+        <p>© 2025 QuickTraderSetup. All rights reserved.</p>
+        <p>Privacy Policy</p>
+        <p>Terms of Service</p>
+        <p>Contact Us</p>
+        <p>Follow us on:</p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/app/Components/Header/Header.jsx
@@ -0,0 +1,55 @@
+import style from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header>
+      <nav className={style.navbar}>
+        <div className={style.logo}>
+          <h1>QuickTraderSetup</h1>
+        </div>
+        <div className={style.contact}>
+          <ul className={style.list}>
+            <li>
+              <a
+                href="https://google.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h2>Home</h2>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://google.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h2>Cheat Scheets</h2>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://google.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h2>Contact</h2>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://google.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h2>
+                  <i class="fa fa-user"></i>
+                </h2>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
