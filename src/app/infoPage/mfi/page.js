43bed639e5e455457c67b1b0eb22a059b67b57e8
Ajgorek04 <igorblendzinski12@gmail.com>
2025-07-10 19:19:48 +0200

    Test

--- src/app/infoPage/mfi/page.js
+++
@@ -1,9 +0,0 @@
-import MfiPageComponent from "../../components/MfiPageComponent/MfiPageComponent";
-
-export default function MfiPage() {
-  return (
-    <>
-      <MfiPageComponent />
-    </>
-  );
-}
